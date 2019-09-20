38c7a3fa17fa97965bc344aab45d0e10b4931942
Chris Linnan <chris.linnan@gmail.com>
2019-09-20 16:34:52 +1000

    Merged chris front end into master

--- client/src/Components/Top Level/HeaderBar/SearchBar.tsx
+++ client/src/Components/Top Level/HeaderBar/SearchBar.tsx
@@ -2,13 +2,12 @@ import React, { useState, useContext } from 'react';
 import SearchContext from "../../../Context/Search/searchContext";
 import "./SearchBar.less";
 import { Input, Button, Form } from 'antd';
-// import { Redirect } from 'react-router-dom';
+import { withRouter } from 'react-router-dom'
 
-const SearchBar = () => {
+const SearchBar = (props: any) => {
+
     const searchContext = useContext(SearchContext);
-
     const [text, setText] = useState('');
-    //const [redirect, setRedirect] = useState(false);
 
     const onSubmit = (e: any) => {
         //stops it from submitting to a file                    
@@ -20,9 +19,8 @@ const SearchBar = () => {
         } else {
             //passes the text up to the app level
             searchContext.searchNodes(text);
-
             setText('');
-            //setRedirect(true);
+            props.history.push('/searchresults');
         }
 
     };
@@ -30,22 +28,14 @@ const SearchBar = () => {
     //Since its only one expression the function can be simplified
     const onChange = (e: any) => setText(e.target.value);
 
-
-        return (
-
-            <div className="searcharea">
-                <Form layout="inline" className="form" onSubmit={onSubmit}>
-    
-                    <Input placeholder="Search" value={text} onChange={onChange} />
-                    <Button type="primary" shape="circle" icon="search" onClick={onSubmit} />
-    
-                </Form>
-            </div>
-    
-        )
-      
-    
+    return (
+        <div className="searcharea">
+            <Form layout="inline" className="form" onSubmit={onSubmit}>
+                <Input placeholder="Search" value={text} onChange={onChange} />
+                <Button type="primary" shape="circle" icon="search" onClick={onSubmit} />
+            </Form>
+        </div>
+    )
 }
 
-
-export default SearchBar
+export default withRouter(SearchBar)
