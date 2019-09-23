df7821472133c3ac80fe279e118ba5e7f48224e5
Luke Daniels <41908057+Lukecam07@users.noreply.github.com>
2019-09-23 22:24:39 +1000

    Test

--- server/src/database/courses.ts
+++ server/src/database/courses.ts
@@ -1,82 +1,33 @@
 import * as mongoose from "mongoose";
 
-/**
- * NodalModel
- *
- * @export
- * @interface NodalModel
- * @extends {mongoose.Document}
- */
-export interface NodalModel extends mongoose.Document {
-    depth: number;
-    name: string;
-    json: string;
-    keywords: string[];
+
+export interface CourseModel extends mongoose.Document {
+    pages: {}[];
+    content: string[];
 }
 
-/**
- * TSNodalModel
- * A model for in memory array use and recursive nesting
- *
- * @export
- * @interface TSNodalModel
- * @extends {NodalModel}
- */
-export interface TSNodalModel extends NodalModel {
+export interface TSCourseModel extends CourseModel {
     uuid: string;
-    children: any[];
-}
-
-/**
- * INodeModel
- *
- * @export
- * @interface INodeModel
- * @extends {NodalModel}
- */
-export interface INodeModel extends NodalModel {
     createdAt: Date;
-    parents: mongoose.Types.ObjectId[];
-    children: mongoose.Types.ObjectId[];
 }
 
-export const NodeSchema: mongoose.Schema = new mongoose.Schema({
+export const CourseSchema: mongoose.Schema = new mongoose.Schema({
     createdAt: {
         type: Date,
         unique: false,
         required: true,
         default: Date.now
     },
-    depth: {
-        type: Number,
+    pages: [{
+        type: {},
         unique: false,
         required: true
-    },
-    name: {
-        type: String,
-        unique: false,
-        required: true
-    },
-    json: {
-        type: String,
-        unique: false,
-        required: true
-    },
-    keywords: {
-        type: [String],
-        unique: false,
-        required: false
-    },
-    parents: [{
-        type: [mongoose.Schema.Types.ObjectId],
-        unique: false,
-        required: false
     }],
-    children: [{
-        type: [mongoose.Schema.Types.ObjectId],
+    content: [{
+        type: String,
         unique: false,
-        required: false
-    }]
+        required: true
+    }],
 });
 
-export const Node: mongoose.Model<INodeModel> = mongoose.model<INodeModel>("Nodes", NodeSchema);
+export const Course: mongoose.Model<CourseModel> = mongoose.model<CourseModel>("Courses", CourseSchema);
