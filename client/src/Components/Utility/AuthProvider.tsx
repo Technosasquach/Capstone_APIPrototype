f2daa0e65d96a561d8c89bd31a223d0862ebb708
Jonn Dillon <jonn_dillon@iinet.net.au>
2019-10-08 09:29:03 +1000

    Changed default settings to make system more streamlined

--- client/src/Components/Utility/AuthProvider.tsx
+++ client/src/Components/Utility/AuthProvider.tsx
@@ -11,7 +11,7 @@ export default class AuthProvider extends React.Component<any, { isAuthorized: b
         super(props);
 
         this.state = {
-            isAuthorized: false,
+            isAuthorized: true,
             errorMsg: ""
         }
 
